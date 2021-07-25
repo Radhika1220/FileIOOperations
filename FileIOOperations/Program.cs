0bda2eb13d48e0353c5c5ae81e911f24e7a1b687
Radhika <radhika.shankar1220@gmail.com>
2021-07-25 12:27:22 +0530

    [Radhika] File exists or not

---
+++ FileIOOperations/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FileIOOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to File IO Operations");
+            string path = @"C:\Users\Radhika\source\repos\FileIOOperations\FileIOOperations\FileText.txt";
+            FileOperations.FilExists(path);
+        }
+    }
+}
